ab87651cfa2a55ebb59ed9f0e2761438af05fbbb
wkdtpgns5016 <wkdtpgns5016@naver.com>
2022-03-22 13:00:55 +0000

    2022-03-22: get_next_line
    2022-03-22 get_next_line 과제수행
    1. 힙 세그먼트와 스택 세그먼트 개념 학습
    2. 읽을 내용이 없으면 null을 반환하도록 반환값 구현
    3. 현재 로직의 문제점 : read시, 버퍼안에 개행이 여러개 있을경우, 백업에 개행이 담기는 문제발생
    	->read전에 버퍼안에 개행있는지 검사해야함

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,22 +1,7 @@
 #include "get_next_line.h"
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
-
-int	check_newline(char *buf, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		if (buf[i] == '\n')
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
 int	get_newline(char *buf, int size)
 {
@@ -32,84 +17,93 @@ int	get_newline(char *buf, int size)
 	return (-1);
 }
 
-char	*get_line(char *buf, char *backup, int size)
+char	*get_line(char *buf, char **backup, int size)
 {
 	int		newline;
 	char	*line;
+	char	*temp;
 
 	newline = get_newline(buf, size);
-	line = (char *)malloc(sizeof(char) * (ft_strlen(backup) + newline + 1));
-	if (line == 0)
+	if ((line = (char *)malloc(sizeof(char) * (ft_strlen(*backup) + newline + 1))) == 0)
 		return (0);
-	ft_strlcat(line, backup, ft_strlen(backup) + 1);
-	ft_strlcat(line, buf, ft_strlen(backup) + newline + 1);
+	ft_strlcat(line, *backup, ft_strlen(*backup) + 1);
+	ft_strlcat(line, buf, ft_strlen(*backup) + newline + 1);
+	temp = *backup;
+	free(temp);
 	return (line);
 }
 
-char	*get_backup(char *buf, char *backup, int size)
+void	set_backup(char *buf, char **backup, int size)
 {
 	int		newline;
 	char	*str;
 
 	newline = get_newline(buf, size);
-	str = ft_strjoin(backup, buf + newline + 1);
-	return (str);
+	str = ft_strjoin(*backup, buf + newline + 1);
+	*backup = str;
 }
 
 char	*get_next_line(int fd)
 {
 	ssize_t		flag;
-	char		buf[BUFFER_SIZE + 1];
+	char		*buf;
 	char		*line;
-	static char	*backup = "";
+	static char	*backup;
 
-	flag = 1;
-	while (flag != 0)
+	line = 0;
+	if ((buf = (char *)malloc(BUFFER_SIZE + 1)) == 0)
+		return (0);
+	if (backup == 0)
 	{
-		flag = read(fd, buf, BUFFER_SIZE);
+		if ((backup = (char *)malloc(sizeof(char))) == 0)
+		return (0);
+	}
+	while ((flag = read(fd, buf, BUFFER_SIZE)) != 0)
+	{
 		buf[flag] = '\0';
-		if (check_newline(buf, (int)flag))
+		if (get_newline(buf, (int)flag) >= 0)
 		{
-			line = get_line(buf, backup, (int)flag);
-			backup = "";
-			backup = get_backup(buf, backup, (int)flag);
+			line = get_line(buf, &backup, (int)flag);
+			set_backup(buf, &backup, (int)flag);
 			return (line);
 		}
 		else
-			backup = get_backup(buf, backup, (int)flag);
+			set_backup(buf, &backup, (int)flag);
 	}
-	return (backup);
+	if (backup != 0)
+	{
+		line = backup;
+		backup = 0;
+	}
+	return (line);
 }
 
-// int main() 
-// { 
-//     int fd; 
-//     int i; 
-//     int j; 
-//     char *line = 0; 
-//     char *lineadress[66]; 
+int main() 
+{ 
+    int fd; 
+    int i; 
+    int j; 
+    char *line = 0; 
+    char *lineadress[66]; 
     
-//     j = 1; 
-//     printf("\n==========================================\n"); 
-//     printf("========= TEST 2 : Empty Lines ===========\n"); 
-//     printf("==========================================\n\n"); 
-//     if (!(fd = open("/home/bigdata/42seoul/gnlTester/files/41_no_nl", O_RDONLY))) 
-//     { 
-//         printf("\nError in open\n"); 
-//         return (0); 
-//     }
-//     printf("%s", get_next_line(fd));
-//     getchar();
-//     printf("%s", get_next_line(fd));
-//     getchar();
-//     printf("%s", get_next_line(fd));
-//     // while ((line = get_next_line(fd)) != 0) 
-//     // { 
-//     //     printf("|%s\n", line); 
-//     //     free(line); 
-//     //     j++; 
-//     // } 
-//     // printf("|%s\n", line); 
-//     // free(line); 
-//     close(fd); 
-// }
+    j = 1; 
+    printf("\n==========================================\n"); 
+    printf("========= TEST 2 : Empty Lines ===========\n"); 
+    printf("==========================================\n\n"); 
+    if (!(fd = open("/home/bigdata/42seoul/gnlTester/files/42_with_nl", O_RDONLY))) 
+    { 
+        printf("\nError in open\n"); 
+        return (0); 
+    }
+    printf("%s\n", get_next_line(fd));
+    printf("%s\n", get_next_line(fd));
+    // while ((line = get_next_line(fd)) != 0) 
+    // { 
+    //     printf("|%s\n", line); 
+    //     free(line); 
+    //     j++; 
+    // } 
+    // printf("|%s\n", line); 
+    // free(line); 
+    close(fd); 
+}

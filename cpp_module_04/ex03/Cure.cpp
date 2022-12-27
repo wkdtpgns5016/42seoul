d009d2b9972becd3d8a1971fe27befe625945a84
Sehoon Jang <sehjang@c1r8s7.42seoul.kr>
2022-12-27 20:35:18 +0900

    2022-12-27: cpp_module_04
    1. ice 와 cure의 출력문 수정

--- cpp_module_04/ex03/Cure.cpp
+++ cpp_module_04/ex03/Cure.cpp
@@ -29,6 +29,6 @@ AMateria* Cure::clone() const
 
 void Cure::use(ICharacter& target)
 {
-	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
 }
 
--- cpp_module_04/ex03/Ice.cpp
+++ cpp_module_04/ex03/Ice.cpp
@@ -29,5 +29,5 @@ AMateria* Ice::clone() const
 
 void Ice::use(ICharacter& target)
 {
-	std::cout << "* heals " << target.getName() << "'s wounds *" << std::endl;
+	std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
 }
